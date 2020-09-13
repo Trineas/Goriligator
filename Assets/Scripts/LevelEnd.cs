4c500756dd860cb9b385d2fefda2a1d822227509
Trineas <danny.sternbeck@gmail.com>
2020-09-14 00:25:22 +0200

    Level 2 adjusted

--- Assets/Scripts/LevelEnd.cs
+++ Assets/Scripts/LevelEnd.cs
@@ -7,9 +7,9 @@ public class LevelEnd : MonoBehaviour
 {
     public string nextLevelToLoad;
 
-    public bool trainActive;
+    public bool trainActive, toiletActive;
 
-    public int trainCrashSound;
+    public int trainCrashSound, toiletFlushSound;
 
     public GameObject player;
 
@@ -40,6 +40,23 @@ public class LevelEnd : MonoBehaviour
         PlayerController.instance.stopMove = false;
     }
 
+    IEnumerator DeathByToiletCo()
+    {
+        UIManager.instance.blackScreen.color = new Color(UIManager.instance.blackScreen.color.r, UIManager.instance.blackScreen.color.g, UIManager.instance.blackScreen.color.b, 1f);
+        AudioManager.instance.PlaySFX(toiletFlushSound);
+        player.transform.position = new Vector3(0f, 10f, 0f);
+
+        yield return new WaitForSeconds(4f);
+        UIManager.instance.fadeFromBlack = true;
+        UIManager.instance.gameOverScreen.gameObject.SetActive(true);
+        yield return new WaitForSeconds(2f);
+        UIManager.instance.fadeToBlack = true;
+        yield return new WaitForSeconds(4f);
+
+        SceneManager.LoadScene(nextLevelToLoad);
+        PlayerController.instance.stopMove = false;
+    }
+
     public void LoadNextLevel()
     {
         StartCoroutine(LoadNextLevelCo());
@@ -48,6 +65,11 @@ public class LevelEnd : MonoBehaviour
     public void DeathByTrain()
     {
         StartCoroutine(DeathByTrainCo());
+    }
+
+    public void DeathByToilet()
+    {
+        StartCoroutine(DeathByToiletCo());
     }
 
     private void OnTriggerEnter(Collider other)
@@ -59,6 +81,10 @@ public class LevelEnd : MonoBehaviour
             if (trainActive)
             {
                 DeathByTrain();
+            }
+            else if (toiletActive)
+            {
+                DeathByToilet();
             }
             else
             {
