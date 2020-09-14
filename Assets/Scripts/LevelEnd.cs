74cc9da830204b8e88ac674a5a73bcaff126815f
Trineas <danny.sternbeck@gmail.com>
2020-09-14 16:48:34 +0200

    Level 4 adjusted

--- Assets/Scripts/LevelEnd.cs
+++ Assets/Scripts/LevelEnd.cs
@@ -7,9 +7,9 @@ public class LevelEnd : MonoBehaviour
 {
     public string nextLevelToLoad;
 
-    public bool trainActive, toiletActive, fireplaceActive;
+    public bool trainActive, toiletActive, fireplaceActive, blenderActive;
 
-    public int trainCrashSound, toiletFlushSound, fireplaceSound;
+    public int trainCrashSound, toiletFlushSound, fireplaceSound, blenderSound;
 
     public GameObject player;
 
@@ -74,6 +74,23 @@ public class LevelEnd : MonoBehaviour
         PlayerController.instance.stopMove = false;
     }
 
+    IEnumerator DeathByBlenderCo()
+    {
+        UIManager.instance.blackScreen.color = new Color(UIManager.instance.blackScreen.color.r, UIManager.instance.blackScreen.color.g, UIManager.instance.blackScreen.color.b, 1f);
+        AudioManager.instance.PlaySFX(blenderSound);
+        player.transform.position = new Vector3(0f, 10f, 0f);
+
+        yield return new WaitForSeconds(4f);
+        UIManager.instance.fadeFromBlack = true;
+        UIManager.instance.gameOverScreen.gameObject.SetActive(true);
+        yield return new WaitForSeconds(2f);
+        UIManager.instance.fadeToBlack = true;
+        yield return new WaitForSeconds(4f);
+
+        SceneManager.LoadScene(nextLevelToLoad);
+        PlayerController.instance.stopMove = false;
+    }
+
     public void LoadNextLevel()
     {
         StartCoroutine(LoadNextLevelCo());
@@ -92,6 +109,10 @@ public class LevelEnd : MonoBehaviour
     public void DeathByFire()
     {
         StartCoroutine(DeathByFireCo());
+    }
+    public void DeathByBlender()
+    {
+        StartCoroutine(DeathByBlenderCo());
     }
 
     private void OnTriggerEnter(Collider other)
@@ -113,6 +134,10 @@ public class LevelEnd : MonoBehaviour
             else if (fireplaceActive)
             {
                 DeathByFire();
+            }
+            else if (blenderActive)
+            {
+                DeathByBlender();
             }
             else
             {
---
+++ Assets/Scripts/OpeningCutscne.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class OpeningCutscne : MonoBehaviour
+{
+    public Image blackScreen;
+    public float blackScreenFadeSpeed;
+    public bool fadeToBlack, fadeFromBlack;
+    public string levelToLoad;
+
+    public Image image1, image2, image3, image4, image5, image6, image7, image8;
+
+    void Start()
+    {
+        PlayCutscene();
+    }
+
+    void Update()
+    {
+        if (fadeToBlack)
+        {
+            blackScreen.color = new Color(blackScreen.color.r, blackScreen.color.g, blackScreen.color.b, Mathf.MoveTowards(blackScreen.color.a, 1f, blackScreenFadeSpeed * Time.deltaTime));
+
+            if (blackScreen.color.a == 1f)
+            {
+                fadeToBlack = false;
+            }
+        }
+
+        if (fadeFromBlack)
+        {
+            blackScreen.color = new Color(blackScreen.color.r, blackScreen.color.g, blackScreen.color.b, Mathf.MoveTowards(blackScreen.color.a, 0f, blackScreenFadeSpeed * Time.deltaTime));
+
+            if (blackScreen.color.a == 0f)
+            {
+                fadeFromBlack = false;
+            }
+        }
+    }
+
+    IEnumerator PlayCutsceneCo()
+    {
+        image1.gameObject.SetActive(true);
+        yield return new WaitForSeconds(4f);
+
+        image2.gameObject.SetActive(true);
+        yield return new WaitForSeconds(2f);
+
+        image3.gameObject.SetActive(true);
+        yield return new WaitForSeconds(2f);
+
+        image4.gameObject.SetActive(true);
+        yield return new WaitForSeconds(2f);
+
+        image5.gameObject.SetActive(true);
+        yield return new WaitForSeconds(2f);
+
+        image6.gameObject.SetActive(true);
+        yield return new WaitForSeconds(2f);
+
+        image7.gameObject.SetActive(true);
+        yield return new WaitForSeconds(3f);
+
+        image8.gameObject.SetActive(true);
+        yield return new WaitForSeconds(4f);
+
+        fadeToBlack = true;
+        yield return new WaitForSeconds(4f);
+
+        SceneManager.LoadScene(levelToLoad);
+    }
+
+    public void PlayCutscene()
+    {
+        StartCoroutine(PlayCutsceneCo());
+    }
+}
